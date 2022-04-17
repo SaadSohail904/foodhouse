71ec92fa4a966984b301d36daaf64a76149764ba
Saad Sohail <saad.sohail@trilogy.com>
2022-04-18 00:54:45 +0500

    logs

--- middleware/authToken.js
+++ middleware/authToken.js
@@ -8,7 +8,8 @@ exports.validateUser = async (req , res  , next)=>{
     if (req.headers.authorization && req.headers.authorization.startsWith("Bearer ")){
       console.log("req.body", req.body)
       console.log("req.files", req.files)
-      console.log("req.headers.authorization", req.headers.authorization)
+      console.log("req.method", req.method)
+      console.log("req.headers", req.headers)
       token = req.headers.authorization.substring(7, req.headers.authorization.length);
       let query = `select u.id as user_id, a.id as token_id, a.end_date from user u inner join authtoken a on u.id = a.user_id where a.token = "${token}"`;
       let queryResults =  await functions.runQuery(query);
