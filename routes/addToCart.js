e8040cfa6f1ac97fdb87039956f6212016d3cca9
Saad Sohail <saad.sohail@trilogy.com>
2022-04-22 00:18:22 +0500

    get cart remove cart_id

--- routes/addToCart.js
+++ routes/addToCart.js
@@ -13,9 +13,16 @@ router.post('/', async function (req, res, next) {
   try{
     let validated = validationSchema.validate(req.body);
     if(!validated.error){
-        var insertionResults = await functions.runQuery(`Insert into cart_food_items_mapper(cart_id, food_item_id, amount) values(${req.body.cart_id}, ${req.body.food_item_id}, ${req.body.amount})`);
-        console.log(insertionResults)
-        res.send({ statusCode: 200, message: "Added to cart succesfully"} );
+      if(!req.body.cart_id){
+        var customerResults = await functions.runQuery(`Select customer.id from customer where customer.user_id = ${req.body.user_id}`)
+        var insertionResults = []
+        if(customerResults.length){
+            insertionResults = await functions.runQuery(`Insert into cart(customer_id, restaurant_id) values(${customerResults[0].id}, ${req.body.restaurant_id})`);
+            req.body.cart_id = insertionResult
+          }
+      }
+      await functions.runQuery(`Insert into cart_food_items_mapper(cart_id, food_item_id, amount) values(${req.body.cart_id}, ${req.body.food_item_id}, ${req.body.amount})`);
+      res.send({ statusCode: 200, message: "Added to cart succesfully"} );
     }else {
         res.send({ statusCode: 405, message: validated.error.message });
     }
--- routes/getCart.js
+++ routes/getCart.js
@@ -4,16 +4,20 @@ const router = express.Router();
 const functions = require('../middleware/functions');
 
 const validationSchema = Joi.object().keys({
-    cart_id: Joi.number().integer().required(), 
     user_id: Joi.number().integer().required()
 });
 router.get('/', async function (req, res, next) {
   try{
     let validated = validationSchema.validate(req.query);
     if(!validated.error){
-        var cartItems = await functions.runQuery(`Select * from food_items f right join cart_food_items_mapper c on f.id = c.food_item_id 
-        where cart_id = ${req.query.cart_id}`);
-        res.send({ statusCode: 200, message: "Data retrieved", data: cartItems} );
+      var customerResults = await functions.runQuery(`Select customer.id from customer where customer.user_id = ${req.query.user_id}`)
+      var cartItems = []
+      if(customerResults.length){
+        cartItems = await functions.runQuery(`Select * from food_items f right join cart_food_items_mapper c on f.id = c.food_item_id right join cart on cart.id = c.cart_id
+        where cart.customer_id = ${customerResults[0].id}`);
+      }
+    
+         res.send({ statusCode: 200, message: "Data retrieved", data: cartItems} );
     }else {
         res.send({ statusCode: 405, message: validated.error.message });
     }
