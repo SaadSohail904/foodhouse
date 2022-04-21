1ce00b5b59550ef2f2aa351939863c65aeee469a
Saad Sohail <saad.sohail@trilogy.com>
2022-04-22 00:35:47 +0500

    addtocart/update cart

--- routes/addToCart.js
+++ routes/addToCart.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const functions = require('../middleware/functions');
 
 const validationSchema = Joi.object().keys({
-    cart_id: Joi.number().integer().required(), 
     food_item_id: Joi.number().integer().required(),
     amount: Joi.number().integer().required(),
     user_id: Joi.number().integer().required()
@@ -13,15 +12,17 @@ router.post('/', async function (req, res, next) {
   try{
     let validated = validationSchema.validate(req.body);
     if(!validated.error){
-      if(!req.body.cart_id){
-        var customerResults = await functions.runQuery(`Select customer.id from customer where customer.user_id = ${req.body.user_id}`)
-        var insertionResults = []
-        if(customerResults.length){
+      var customerResults = await functions.runQuery(`Select customer.id from customer where customer.user_id = ${req.body.user_id}`)
+      var insertionResults = []
+      if(customerResults.length){
+          req.body.cart_id = await cartExists(customerResults[0].id)
+          if(!req.body.cart_id){
             insertionResults = await functions.runQuery(`Insert into cart(customer_id, restaurant_id) values(${customerResults[0].id}, ${req.body.restaurant_id})`);
-            req.body.cart_id = insertionResult
+            req.body.cart_id = insertionResults.insertId;
           }
       }
-      await functions.runQuery(`Insert into cart_food_items_mapper(cart_id, food_item_id, amount) values(${req.body.cart_id}, ${req.body.food_item_id}, ${req.body.amount})`);
+      console.log(`Insert into cart_food_items_mapper(cart_id, food_item_id, amount) values(${req.body.cart_id}, ${req.body.food_item_id}, ${req.body.amount}) ON DUPLICATE KEY UPDATE amount=values(amount)`)
+      await functions.runQuery(`Insert into cart_food_items_mapper(cart_id, food_item_id, amount) values(${req.body.cart_id}, ${req.body.food_item_id}, ${req.body.amount}) ON DUPLICATE KEY UPDATE amount=values(amount)`);
       res.send({ statusCode: 200, message: "Added to cart succesfully"} );
     }else {
         res.send({ statusCode: 405, message: validated.error.message });
@@ -34,5 +35,14 @@ router.post('/', async function (req, res, next) {
       }
   }
 })
+async function cartExists(customer_id){
+  let cartResults = await functions.runQuery(`Select id from cart where customer_id = ${customer_id}`)
+  console.log(cartResults)
+  if(cartResults.length){
+    return cartResults[0].id
+  } else{
+    return false;
+  }
+}
 
 module.exports = router;
