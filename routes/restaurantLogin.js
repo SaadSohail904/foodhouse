ca3611af3e6fa21a0caf525617b879e432bd8277
Saad Sohail <saad.sohail@trilogy.com>
2022-05-01 01:47:40 +0500

    restaurantLogin and signup

---
+++ routes/restaurantLogin.js
@@ -0,0 +1,64 @@
+var bcrypt = require('bcryptjs');
+const express = require('express');
+const Joi = require('joi');
+const crypto = require('crypto');
+const router = express.Router();
+const functions = require('../middleware/functions');
+const pool = require("../db.js");
+const roles = require('../middleware/roles');
+
+
+const validationSchema = Joi.object().keys({
+  email: Joi.string().required(),
+  password: Joi.string().required()
+});
+router.post('/', async function (req, res, next) {
+  let validated = validationSchema.validate(req.body, { abortEarly: false });
+  try {
+    pool.getConnection(function(err, con) {
+      if(err){
+        console.log(err);
+        return res.send({ statusCode: 405, message: "Error connecting to database" });
+      }
+      con.beginTransaction(async function () {
+        try {
+          if (!validated.error) {
+            req.body.email = req.body.email.toLowerCase();
+            let restaurant = await functions.runTransactionQuery(`SELECT restaurant.id as restaurant_id, * FROM restaurant inner join
+            user on restaurant.user_id = user.id WHERE email = "${req.body.email}"`, con)
+            if (restaurant.length) {
+              let token = crypto.randomBytes(32).toString('hex');
+              let expiry_time = 1000*60*60*24*7;
+              restaurant[0].role = roles[1]
+              if (req.body.password != restaurant[0].password){
+                res.send({ statusCode: 405, message: "Invalid credentials" })
+              } else {
+                  let query = `Insert into authtoken (token , user_id, start_date, end_date) values("${token}" ,${restaurant[0].user_id},
+                  CURRENT_TIMESTAMP, DATE_ADD(CURRENT_TIMESTAMP,  INTERVAL 7 DAY))`;
+                  await functions.runTransactionQuery(query, con);
+                  con.commit();
+                  console.log(`Logged in restaurant ${restaurant[0].restaurant_id} at ${new Date}`);
+                  res.send({ statusCode: 200, restaurant: restaurant[0], message: "Logged in successfully", token: token, expiry_time: expiry_time});
+                
+              }
+            } else {
+              con.rollback();
+              res.send({ statusCode: 405, message: "Email not registered" });
+            }
+          } else {
+            con.rollback();
+            res.send({ statusCode: 405, message: validated.error.message })
+          }
+        } catch (err) {
+          con.rollback();
+          res.send({ statusCode: 405, message: err.message });
+        }
+      })
+    });
+  } catch (err) {
+    con.rollback();
+    res.send({ statusCode: 405, message: err.message })
+  }
+})
+
+module.exports = router;
---
+++ routes/restaurantSignup.js
@@ -0,0 +1,55 @@
+const mysql = require("mysql");
+const express = require('express');
+const Joi = require('joi');
+const router = express.Router();
+const functions = require('../middleware/functions');
+const pool = require("../db.js");
+
+
+
+router.post('/', function (req, res) {
+  pool.getConnection(function(err, con) {
+    if(err){
+      console.log(err);
+      return res.send({ statusCode: 405, message: "Error connecting to database" });
+    }
+    con.beginTransaction(async function () {
+      try {
+  			let validationSchema = Joi.object().keys({
+  			  fname: Joi.string().required(),
+  			  lname: Joi.string().required(),
+  			  email: Joi.string().email().required(),
+  			  mobileno: Joi.number().integer().required(),
+  			  password: Joi.string().required(),
+  			  gender: Joi.number().integer().required().valid(0, 1)
+  			  // age: Joi.number().integer().required(),
+  			});
+  			let validation = validationSchema.validate(req.body, { abortEarly: false });
+        if (!validation.error) {
+  				// var age = req.body.age;
+          req.body.email = req.body.email.toLowerCase();
+          let currentTime = new Date().toISOString().slice(0, 19).replace('T', ' ');
+          var userResults = await functions.runTransactionQuery(`Insert into user(role) values(0)`, con);
+          var queryResults = await functions.runTransactionQuery(`Insert into restaurant(name, password, owner, mobileno, email, address, user_id) values("${req.body.name}",
+          "${req.body.password}", "${req.body.owner}", ${req.body.mobileno}, "${req.body.email}", ${req.body.address},  ${userResults.insertId})`, con);
+          console.log(`Registered user ${queryResults.insertId} at ${new Date()}`)
+        	con.commit();
+            res.send({statusCode: 200, message:"Signed up successfully"});
+  			} else {
+          con.rollback();
+          res.send({ statusCode: 405, message: validation.error.message });
+        }
+      } catch (err) {
+        con.rollback();
+  			console.log("Error in signup:", err);
+        if (err.code == 'ER_DUP_ENTRY') {
+          res.send({ statusCode: 405, message: 'Email already registered' });
+        } else {
+          res.send({ statusCode: 405, message: err.message });
+        }
+      }
+    })
+  })
+})
+
+module.exports = router;
