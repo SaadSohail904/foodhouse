31ad8861a9d446ef26c7c7f05b24bcfe8fc64c34
Saad Sohail <saad.sohail@trilogy.com>
2022-05-16 20:31:15 +0500

    misc

--- routes/addOrder.js
+++ routes/addOrder.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const functions = require('../middleware/functions');
 
 const validationSchema = Joi.object().keys({
+    restaurant_id: Joi.number().integer().required(), 
     user_id: Joi.number().integer().required()
 });
 router.post('/', async function (req, res, next) {
@@ -13,7 +14,7 @@ router.post('/', async function (req, res, next) {
       var customerResults = await functions.runQuery(`Select customer.id from customer where customer.user_id = ${req.body.user_id}`)
       if(customerResults.length){
         var foodItemResults = await functions.runQuery(`Select distinct(f.id), r.id as restaurant_id from food_items f inner join cart_food_items_mapper m on m.food_item_id = f.id 
-        inner join restaurants r on f.restaurant_id = r.id inner join cart c on c.id = m.cart_id where c.id in ${customerResults[0].id})`);
+        inner join restaurants r on f.restaurant_id = r.id inner join cart c on c.id = m.cart_id where c.id in (Select id from cart where customer_id = ${customerResults[0].id})`);
         console.log(foodItemResults)
         if(foodItemResults.length){
           var insertionResults = await functions.runQuery(`Insert into orders(customer_id, restaurant_id, status, created_at) values(${customerResults[0].id}, ${foodItemResults[0].restaurant_id}, "Pending", CURRENT_TIMESTAMP)`);
