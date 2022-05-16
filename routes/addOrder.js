35ccad8fccfb6d3b00377a086ab458c9867a792c
Saad Sohail <saad.sohail@trilogy.com>
2022-05-16 18:50:31 +0500

    add order api

--- routes/addOrder.js
+++ routes/addOrder.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const functions = require('../middleware/functions');
 
 const validationSchema = Joi.object().keys({
-    food_item_id: Joi.number().integer().required(), 
+    restaurant_id: Joi.number().integer().required(), 
     user_id: Joi.number().integer().required()
 });
 router.post('/', async function (req, res, next) {
@@ -13,18 +13,14 @@ router.post('/', async function (req, res, next) {
     if(!validated.error){
       var customerResults = await functions.runQuery(`Select customer.id from customer where customer.user_id = ${req.body.user_id}`)
       if(customerResults.length){
-        var insertionResults = await functions.runQuery(`Insert into favourites_customer_food_mapper(customer_id, food_item_id) values(${customerResults[0].id}, ${req.body.food_item_id})`);
+        var insertionResults = await functions.runQuery(`Insert into orders(customer_id, restaurant_id, status, created_at) values(${customerResults[0].id}, ${req.body.restaurant_id}, "Pending", CURRENT_TIMESTAMP)`);
       }
       res.send({ statusCode: 200, message: "Added succesfully"} );
     }else {
         res.send({ statusCode: 405, message: validated.error.message });
     }
   } catch(error) {
-    if(error.code=="ER_DUP_ENTRY"){
-      res.send({"statusCode": 405, "message": "You have already added that item to favourites"});
-    } else{
       res.send({"statusCode": 405, "message": error.message});
-    }
   }
 })
 
