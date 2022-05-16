ad167a499e3fa567aa8e6f97d8bc00066ada7950
Saad Sohail <saad.sohail@trilogy.com>
2022-05-16 20:06:10 +0500

    misc

--- routes/getRestaurantOrders.js
+++ routes/getRestaurantOrders.js
@@ -16,7 +16,7 @@ router.get('/', async function (req, res, next) {
         if(restaurantResults.length){
           ordersResults = await functions.runQuery(`Select o.id as order_id, o.status, o.created_at, o.customer_id as customer_id, c.fname, c.lname,
           o.restaurant_id as restaurant_id, f.name, f.id as food_item_id, f.price, f.image, f.category_id, r.name as restaurant_name, u.image as restaurant_image from orders o inner join order_food_items_mapper m on o.id = m.order_id inner join food_items f
-          on m.food_item_id = f.id inner join restaurants r on r.id = f.restaurant_id inner join user u on u.id = r.user_id inner join customer c on c.user_id = u.id where o.restaurant_id = ${restaurantResults[0].id}`);
+          on m.food_item_id = f.id inner join restaurants r on r.id = f.restaurant_id inner join user u on u.id = r.user_id inner join customer c on c.id = o.customer_id where o.restaurant_id = ${restaurantResults[0].id}`);
         }
         let ordersArray = _.uniqBy(ordersResults, "order_id");
         console.log(JSON.stringify(ordersArray))
