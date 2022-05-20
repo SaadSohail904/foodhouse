01dd8404f2388c54065a304800eae4559862453e
Saad Sohail <saad.sohail@trilogy.com>
2022-05-21 02:09:45 +0500

    misc

--- routes/getRestaurantsAndCategories.js
+++ routes/getRestaurantsAndCategories.js
@@ -17,7 +17,17 @@ router.get('/', async function (req, res, next) {
     if(!validated.error){
       var restaurantResults = await functions.runQuery(`Select *, restaurants.id as restaurant_id from restaurants inner join user on restaurants.user_id = user.id  where name like "%${req.query.filter}%"`);
       var categoryResults = await functions.runQuery(`Select * from categories where name like "%${req.query.filter}%"`);
-        res.send({ statusCode: 200, message: "Data retrieved", data: {categories: categoryResults, restaurants: restaurantResults} });
+      if(checkIfAdmin){
+        var restaurantRatings = await functions.runQuery(`Select SUM(rating) as ratingSum, COUNT(orders.id) as ordersCount, restaurant_id from orders inner join restaurants on restaurants.id = orders.restaurant_id group by orders.restaurant_id `)
+        for(let originalRestaurant in restaurantResults){
+          for(let ratingRestaurant in restaurantRatings){
+            if(ratingRestaurant.restaurant_id === originalRestaurant.restaurant_id){
+              originalRestaurant.rating = Math.round(parseFloat(ratingRestaurant.ratingSum)/parseFloat(ratingRestaurant.ordersCount))
+            }
+          }
+        }
+      }
+      res.send({ statusCode: 200, message: "Data retrieved", data: {categories: categoryResults, restaurants: restaurantResults} });
     }else {
         res.send({ statusCode: 405, message: validated.error.message });
     }
@@ -26,4 +36,15 @@ router.get('/', async function (req, res, next) {
   }
 })
 
+async function checkIfAdmin(id){
+  let adminsResults = await functions.runQuery(`Select * from admin`)
+  if(adminsResults.length){
+    for(let admin of adminsResults){
+      if(id == admin.user_id){
+        return true
+      }
+    }
+  }
+  return false
+}
 module.exports = router;
